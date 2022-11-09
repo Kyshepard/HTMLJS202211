394cabdaa2b1a1741e73be96947df149c7c59b1b
Kodiak <kyshepard@email.neit.edu>
2022-11-09 13:31:32 -0500

    sad game

---
+++ RPSDemo.html/rps.js
@@ -0,0 +1,77 @@
+//canvas drawubg stuff
+var canvas = document.getElementById("c");
+var ctx = canvas.getContext("2d");
+
+//drawing the fonts
+ctx.font = "40px Arial";
+ctx.fillStyle = "blue";
+ctx.strokeStyle= "yellow";
+ctx.fillText("Welcome to the RPS Game!", 125, 280);
+ctx.strokeText("Welcome to the RPS Game!", 125, 280);
+
+//alert("select rock, paper, or scissors!");
+var rps = ["rock", "paper", "scissors"];
+alert("select rock, paper, or scissors!");
+//console.log(rps[2]);
+
+document.getElementById("rock").addEventListener('click', function (e) {
+    alert("you picked " + rps[0]);
+    playGame(rps[0]);
+});
+document.getElementById("paper").addEventListener('click', function (e) {
+    alert("you picked " + rps[1]);
+    playGame(rps[1]);
+});
+document.getElementById("scissors").addEventListener('click', function (e) {
+    alert("you picked " + rps[2]);
+    playGame(rps[2]);
+});
+
+function playGame(playerChoice) {
+    var cpuChoice = Math.floor(Math.random() * 2.99);
+    console.log(cpuChoice, playerChoice);
+
+    switch (playerChoice) {
+        case "rock":
+            if (cpuChoice == 0) {
+                //rock
+                alert("CPU chose rock. It's a tie!")
+            }
+            else if (cpuChoice == 1) {
+                //paper
+                alert("CPU chose Paper. You lose!")
+            }
+            else {
+                alert("CPU chose Scissors. You win!")
+            }
+            break;
+
+        case "paper":
+            if (cpuChoice == 0) {
+                //rock
+                alert("CPU chose rock. You win!")
+            }
+            else if (cpuChoice == 1) {
+                //paper
+                alert("CPU chose Paper. It's a tie!")
+            }
+            else {
+                alert("CPU chose Scissors. You lose!")
+            }
+            break;
+
+        case "scissors":
+            if (cpuChoice == 0) {
+                //rock
+                alert("CPU chose rock. You lose!")
+            }
+            else if (cpuChoice == 1) {
+                //paper
+                alert("CPU chose Paper. You win!")
+            }
+            else {
+                alert("CPU chose Scissors. It's a tie!")
+            }
+            break;
+    }
+}
