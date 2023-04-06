e2cd52c798ab4e01c55bc33741da0c35e60864b6
Kodiak <kyshepard@email.neit.edu>
2023-04-06 13:04:29 -0400

    week2

---
+++ gds137/Week 2/js/week2_keyboard_movement.js
@@ -0,0 +1,41 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new GameObject();
+
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	
+	//Move the Player to the right
+	if(d)
+	{
+		console.log("Moving Right");
+		player.x += 2;
+	}
+	if(a)
+	{
+		console.log("Moving Left");
+		player.x += -2;
+	}
+	
+	//Update the Screen
+	player.drawRect();
+}
+
