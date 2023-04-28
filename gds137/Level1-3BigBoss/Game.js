77816d0bc63361e98a4f62b536f8af92427464e7
Kodiak <kyshepard@email.neit.edu>
2023-04-28 13:24:20 -0400

    all I need to do now is the three parts

--- gds137/Level1-3BigBoss/Game.js
+++ gds137/Level1-3BigBoss/Game.js
@@ -6,8 +6,16 @@ var timer;
 var interval = 1000/60;
 var ball;
 var paddle;
-var score;
-var ball
+var score = 0;
+
+//For gravity
+var gravity = 1;
+
+//For aceleration
+
+//For friction
+var fX = .97;
+var fY = .97;
 
 //Setting up the canvas
 canvas = document.getElementById("canvas");
@@ -20,9 +28,9 @@ ball.height = ball.width;
 ball.color = `#ff00ff`;
 ball.vx = 5;
 ball.vy = 0;
-gravity = 1;
 ball.x = canvas.width/2
 ball.y = canvas.height/2
+
 
 //Instantiate the paddle
 paddle = new GameObject();
@@ -47,15 +55,27 @@ ball.move();
 	if(a)
 	{
 		console.log("Moving Left");
-		paddle.x += -8;
+		paddle.vx += -paddle.ax * paddle.force;
+		//paddle.x += -8;
 	}
 	if(d)
 	{
 		console.log("Moving right");
-		paddle.x += 8;
+		paddle.vx += paddle.ax * paddle.force;
+		//paddle.x += 8;
 	}
 
-	//collision for the paddle
+	//paddle aceleration
+	paddle.vx *= fX;
+	paddle.vy *= fY;
+
+	paddle.x += Math.round(paddle.vx);
+	paddle.y += Math.round(paddle.vy);
+
+	//ball gravity
+	ball.vy += gravity;
+
+	//collision for the paddle with walls
 	if(paddle.x < paddle.width/2)
 	{
 		paddle.x = paddle.width/2
@@ -71,45 +91,56 @@ ball.move();
 	}
 
 	//Collision for the ball
-if(ball.x < canvas.wdith/2 - ball.width/2)
+if(ball.x < canvas.width/2 - 500 + ball.width/2)
 {
-	
-	ball.x = canvas.width/2;
-	ball.vx = -ball.vx
+	ball.x = ball.width/2;
+	ball.vx = -ball.vx * .67;
 }
 if(ball.x > canvas.width - ball.width/2)
 {
 	ball.x = canvas.width - ball.width/2;
-	ball.vx = -ball.vx
+	ball.vx = - ball.vx * .67;
 }
 
 if(ball.y < ball.width/2)
 {
 	ball.y = ball.width/2
-	ball.vy = -ball.vy;
+	ball.vy = -ball.vy * .67;
 }
 
 if(ball.y > canvas.height - ball.width/2)
 {
 	ball.y = canvas.height -ball.width/2
-	ball.vy = -ball.vy;
+	ball.vy = -ball.vy * .67;
+	score = 0;
 }
 
 //collisions for ball hiting paddle
 if(ball.hitTestObject(paddle))
 	{
-		ball.x = paddle.x + paddle.width/2 + ball.width/2
-		ball.vx = -ball.vx
+		ball.y = paddle.y - paddle.height/2 - ball.height/2
+		ball.vy = -ball.vy;
+		score = score + 1;
 
-		if(ball.y < player1.y - player1.height /6)
+		if(ball.x < paddle.x/3)
 		{
 		
-			ball.vy = -3
+			ball.vy = -35
+			ball.vx = 5
 		}
-		if (ball.y > paddle.y + paddle.height/6)
+		//inner /6
+		if(ball.x < paddle.x/6)
+		{
+		
+			ball.vy = -35
+			ball.vx = 5
+		}
+		//outer /6
+		if (ball.x < paddle.x/6)
 		{
 			
-			ball.vy =  3
+			ball.vy =  -35
+			ball.vx = 5
 		}
 		
 	}
